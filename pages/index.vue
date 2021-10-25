9ec96d44bbbdd990cbb7c7abe452f889aad4f03a
Pedro Collin <pedrocollintkd@gmail.com>
2021-10-25 11:31:39 -0300

    ProjNxT

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Button label="Primary" />
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+import Tutorial from '../components/Tutorial.vue'
+export default {
+  components: { Tutorial },}
+</script>
